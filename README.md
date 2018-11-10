7c9ec896f7c395ab756b4e96861d9a1ec8b6a7fc
postolenko <denis.postolenko@gmail.com>
2018-11-10 21:07:41 +0200

    PS

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 # http://postolenko.github.io/playstation/index_2.html
 # http://postolenko.github.io/playstation/index_3.html
 # http://postolenko.github.io/playstation/index_4.html
+# http://postolenko.github.io/playstation/dashboard.html
--- js/scripts.js
+++ js/scripts.js
@@ -27,6 +27,109 @@ $(window).resize(function() {
 
 $(document).ready(function() {   
 
+	$(".show_popup").click(function(e) {
+
+        e.preventDefault();
+
+        popupName = $(this).attr("data-popup-name");
+        popupBlock = $("[data-popup = '"+ popupName +"']");
+
+        popupBlock.fadeIn(400);
+
+    });
+
+     $(this).keydown(function(eventObject){
+
+        if (eventObject.which == 27) {
+
+            if ( $(".popup_wrapp").is(":visible") ) {
+
+                $(".popup_wrapp").fadeOut(300);
+
+            }
+
+        }
+
+    });
+
+    $(".close-popup").click(function() {
+
+        popupBlock = $(this).closest(".popup_wrapp");
+
+        popupBlock.fadeOut(300);
+
+    });
+
+
+    $(document).mouseup(function (e){
+
+        hide_element = $('.popup');
+
+        if (!hide_element.is(e.target)
+
+            && hide_element.has(e.target).length === 0) {
+
+            hide_element.closest(".popup_wrapp").fadeOut(300);
+        }
+
+    });
+
+    // ---------------------
+
+    $(".dropdown_wrapp").each( function() {
+
+        $(this).find(".dropdown_list").css({
+            "display" : "none"
+        });
+
+    });
+
+    $(".dropdown_title").click(function(e) {
+
+        e.preventDefault();
+
+        parentBlock = $(this).closest(".dropdown_wrapp");
+
+        var dropdownList = parentBlock.find(".dropdown_list");
+
+        if( dropdownList.is(":hidden") ) {
+
+            dropdownList.slideDown(300);
+            parentBlock.addClass("active");
+
+        } else {
+
+            dropdownList.slideUp(300);
+            parentBlock.removeClass("active");
+
+        }
+
+    });
+
+    $(document).mouseup(function (e){
+
+        hide_element = $('.dropdown_list');
+
+        if (!hide_element.is(e.target)
+
+            && hide_element.has(e.target).length === 0) {
+
+            parentBlock = hide_element.closest(".dropdown_wrapp");
+            hide_element.slideUp(300);
+            parentBlock.removeClass("active");
+        }
+
+    });
+
+    $(this).keydown(function(eventObject){
+
+        if (eventObject.which == 27) {
+
+            $('.dropdown_list').slideUp(300);
+
+        }
+
+    });
 
 });
 
